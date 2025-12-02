f49b7585c21320cf0f584d045938087fa46b4900
JesseJay928 <kaluc935@gmail.com>
2025-12-02 18:44:24 +0100

    Added Flask app and Dockerfile

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-from flask import Flask, request, jsonify
-
+from flask import Flask
 app = Flask(__name__)
 
-todos = []
+@app.route("/")
+def home():
+    return "Hello from your DevOps CI/CD Pipeline!"
 
-@app.route('/todos', methods=['GET'])
-def get_todos():
-    return jsonify(todos)
-
-@app.route('/todos', methods=['POST'])
-def add_todo():
-    data = request.get_json()
-    todos.append(data)
-    return jsonify({"message": "Todo added!"}), 201
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
